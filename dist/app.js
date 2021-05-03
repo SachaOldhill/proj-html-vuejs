79afcd6e86afc9296de0946030c44372413f92db
Sacha <sachacollev@gmail.com>
2021-05-03 12:39:15 +0200

    push

--- dist/app.js
+++ dist/app.js
@@ -20,10 +20,56 @@ function initVue() {
       }, {
         "name": "Courses"
       }],
-      'activeIndex': ''
+      text: [{
+        'h1': 'inspire',
+        'p': 'text lorem ipsum'
+      }, {
+        'h1': 'leaaaaaa',
+        'p': 'aaaaaaaa ipsum'
+      }, {
+        'h1': 'Lead. Inspire. Win',
+        'p': 'At EduPrime, it doesn’t matter what domain you wish to pursue a career in. Here you can find a course that satisfies your needs within a click away and applies for a course'
+      }],
+      'activeIndex': 0,
+      'currentImg': 0,
+      'arrayImg': ['edu/img/slider1.jpg', 'edu/img/slider2.jpg', 'edu/img/slider3.jpg'],
+      'arrayTxt': ['edu/img/slider1.jpg', 'edu/img/slider2.jpg', 'edu/img/slider3.jpg'],
+      'nextImg': 'next',
+      'prevImg': 'prev',
+      'timer': null
     },
     computed: {},
     methods: {
+      go_next: function go_next() {
+        this.currentImg++;
+
+        if (this.currentImg >= this.arrayImg.length) {
+          this.currentImg = 0;
+        }
+
+        console.log('go next');
+      },
+      go_prev: function go_prev() {
+        this.currentImg--;
+        this.activeIndex--;
+
+        if (this.currentImg < 0) {
+          this.currentImg = this.arrayImg.length - 1;
+        }
+
+        if (this.activeIndex < 0) {
+          this.activeIndex = this.arrayTxt.length - 1;
+        }
+
+        console.log('go prev');
+      },
+      intervalCounter: function intervalCounter() {
+        if (this.timer == null) {
+          this.timer = setInterval(this.go_next, 3000);
+        }
+
+        console.log('counter');
+      },
       barHover: function barHover(bar) {
         this.activeIndex = this.nav.indexOf(bar);
         console.log('enter');
